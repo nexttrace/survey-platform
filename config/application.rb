77b4e5d710a9b6df58449a2ddd4ca8413677a41f
Andre Arko <andre@arko.net>
2020-04-18 01:09:19 -0700

    delete sprockets leftovers

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,7 @@ module NexttraceOrg
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
-    # Don't generate system test files.
     config.generators.system_tests = nil
+    config.generators.assets = false
   end
 end
