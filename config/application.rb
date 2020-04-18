0bd800622d73fa280edd35debeabf6810dad1642
Andre Arko <andre@arko.net>
2020-04-18 08:09:39 +0000

    remove yet more generated bits

--- config/application.rb
+++ config/application.rb
@@ -5,11 +5,11 @@ require "rails"
 require "active_model/railtie"
 require "active_job/railtie"
 require "active_record/railtie"
-require "active_storage/engine"
+# require "active_storage/engine"
 require "action_controller/railtie"
 require "action_mailer/railtie"
-require "action_mailbox/engine"
-require "action_text/engine"
+# require "action_mailbox/engine"
+# require "action_text/engine"
 require "action_view/railtie"
 # require "action_cable/engine"
 # require "sprockets/railtie"
@@ -19,7 +19,7 @@ require "action_view/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module NexttraceOrg
+module NextTrace
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
@@ -29,7 +29,11 @@ module NexttraceOrg
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
-    config.generators.system_tests = nil
-    config.generators.assets = false
+    config.generators do |generate|
+      generate.assets false
+      generate.helper false
+      generate.system_tests false
+      generate.view_specs false
+    end
   end
 end
