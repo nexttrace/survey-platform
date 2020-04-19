b31219875636e96d821271db12dad1ea339bc07f
Andre Arko <andre@arko.net>
2020-04-18 23:19:51 -0700

    now it looks like NextTrace

---
+++ app/javascript/src/application.js
@@ -0,0 +1,8 @@
+import $ from "jquery"
+
+$(document).ready(function() {
+  $(".navbar-burger").click(function() {
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
+  });
+});
