925e8100608130789babe3d8ae929c373836eef9
Andre Arko <andre@arko.net>
2020-04-22 13:11:05 -0700

    bring back the response landing page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   get "response/thank_you", to: "responses#thank_you"
   get "response/:step", to: "responses#show", as: :response_step
+  resource :response, only: [:show]
 
   # Public health agencies
   devise_for :users, at: "/agency", controllers: {
