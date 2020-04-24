9f0438b772b6fa369ed6d5b1b9cbda3f164bc891
Andre Arko <andre@arko.net>
2020-04-23 20:47:12 -0700

    use sendgrid when deployed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,14 +100,15 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   unless ENV["ASSET_COMPILATION"]
-    ActionMailer::Base.smtp_settings = {
-      :port           => ENV.fetch('MAILGUN_SMTP_PORT'),
-      :address        => ENV.fetch('MAILGUN_SMTP_SERVER'),
-      :user_name      => ENV.fetch('MAILGUN_SMTP_LOGIN'),
-      :password       => ENV.fetch('MAILGUN_SMTP_PASSWORD'),
-      :domain         => "nexttrace.org",
-      :authentication => :plain,
-    }
     ActionMailer::Base.delivery_method = :smtp
+    ActionMailer::Base.smtp_settings = {
+      :address              => "smtp.sendgrid.net",
+      :port                 => 465,
+      :user_name            => "apikey",
+      :password             => ENV.fetch("SENDGRID_API_KEY"),
+      :domain               => "nexttrace.org",
+      :authentication       => :plain,
+      :enable_starttls_auto => true
+    }
   end
 end
