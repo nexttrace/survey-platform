9eca6c75f243f527b88b32feaff2109b2b569012
Brad Greenlee <brad@footle.org>
2020-04-29 22:17:58 -0700

    [#8 - WIP] Save survey form fields to data

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,6 +1,6 @@
 class SurveysController < RespondentController
   before_action :require_respondent!
-  before_action :set_survey, only: [:show, :thank_you]
+  before_action :set_survey, only: [:show, :thank_you, :update]
 
   # # GET /surveys
   # def index
@@ -34,14 +34,22 @@ class SurveysController < RespondentController
   #   end
   # end
 
-  # # PATCH/PUT /surveys/1
-  # def update
-  #   if @survey.update(survey_params)
-  #     redirect_to @survey, notice: 'Survey was successfully updated.'
-  #   else
-  #     render :edit
-  #   end
-  # end
+  # PATCH/PUT /surveys/1
+  def update
+    logger.debug "params: #{params.inspect}"
+    logger.debug "survey: #{@survey.attributes.inspect}"
+
+    # no step, just boot them to the start of the form
+    return redirect_to(survey_step_path(1)) unless params.has_key?(:step) && (1..7).include?(params[:step].to_i)
+
+    # for now, assume we just go to step n+1; need to handle last step case
+    logger.debug "survey params: #{survey_params}"
+    if @survey.update(survey_params)
+      return redirect_to(survey_step_path(params[:step].to_i + 1))
+    else
+      redirect_to @survey, notice: 'There was a problem saving.' # TODO: this is not right
+    end
+  end
 
   # # DELETE /surveys/1
   # def destroy
