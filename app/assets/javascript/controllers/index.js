90bd41ed1309e9a34ccf1821ff0db0943e7e0b69
Brad Greenlee <brad@footle.org>
2020-05-02 14:59:01 -0700

    Remove debug logging

--- app/assets/javascript/controllers/index.js
+++ app/assets/javascript/controllers/index.js
@@ -1,7 +1,5 @@
 // Load all the controllers within this directory and all subdirectories.
 // Controller files must be named *_controller.js.
-console.log("controller index")
-
 import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
 
