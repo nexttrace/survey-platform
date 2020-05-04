5e8177714ad392dd1965e1fabe1b6b398fc67c29
Andre Arko <andre@arko.net>
2020-05-04 03:10:17 +0000

    clarify language around link when logged out

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -12,7 +12,7 @@ private
 
   def require_contact!
     return if current_contact
-    redirect_to contacts.sign_in_path, flash: { error: "You need to be invited to take the survey" }
+    redirect_to contacts.sign_in_path, flash: { error: "You need a new link to take the survey" }
   end
 
 end
